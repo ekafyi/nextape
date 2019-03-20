ce0d2a5ae3669e0bda94c52e35713792c5279c2a
Eka <eka@deadmediafm.org>
2019-03-20 13:00:00 +0700

    change getInitialProps code

--- pages/index.js
+++ pages/index.js
@@ -2,33 +2,32 @@ import React from 'react';
 import Link from 'next/link';
 import PropTypes from 'prop-types';
 import fetch from 'isomorphic-unfetch';
+import Error from 'next/error';
 
 
 class IndexPage extends React.Component {
 
   // v1 
   // https://medium.com/crowdbotics/how-to-build-a-hacker-news-app-with-react-and-next-js-5fe0c5a64c12
-	// static async getInitialProps() {
-	// 	let stories;
-	// 	try {
-	// 		const response = await fetch(
-	// 			'https://node-hnapi.herokuapp.com/news?page=1'
-	// 		);
-	// 		stories = await response.json();
-	// 	} catch (err) {
-	// 		console.log(err);
-	// 		stories = [];
-	// 	}
-	// 	return { stories };
-	// }
+	static async getInitialProps() {
+		let stories;
+		try {
+			const response = await fetch('https://node-hnapi.herokuapp.com/news?page=1');
+			stories = await response.json();
+		} catch (err) {
+			console.log(err);
+			stories = [];
+		}
+		return { stories };
+	}
 
   // v2
   // https://dev.to/aurelkurtula/introduction-to-the-basics-of-nextjs---part-two-pad
-  static async getInitialProps() {
-    const res = await fetch('https://node-hnapi.herokuapp.com/news?page=1')
-    const stories = await res.json()
-    return { stories }
-  }
+  // static async getInitialProps() {
+  //   const response = await fetch('https://node-hnapi.herokuapp.com/news?page=1')
+  //   const stories = await response.json()
+  //   return { stories }
+  // }
 
 	render() {
     const { stories } = this.props;
